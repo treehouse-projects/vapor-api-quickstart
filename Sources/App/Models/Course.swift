e6921d2063db41f494f2e38a678978543682bde4
Pasan Premaratne <pasan@premaratne.me>
2018-08-20 09:26:06 -0400

    V5

--- Sources/App/Models/Course.swift
+++ Sources/App/Models/Course.swift
@@ -14,6 +14,7 @@ final class Course: Codable {
 
 extension Course: SQLiteModel {}
 extension Course: Migration {}
+extension Course: Content {}
 
 
 
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,5 +2,14 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    // MARK: - Course Routes
     
+    // POST /api/courses
+    // Create a new course. Course info in JSON body
+    
+    router.post("api", "courses") { req -> Future<Course> in
+        return try req.content.decode(Course.self).flatMap(to: Course.self) { course in
+            return course.save(on: req)
+        }
+    }
 }
