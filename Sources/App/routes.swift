6ab48cfa8ca3d6861dd1e2b85e6c93a20816dc50
Pasan Premaratne <pasan@premaratne.me>
2018-08-22 09:25:14 -0400

    V8

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -91,6 +91,45 @@ public func routes(_ router: Router) throws {
             }
         }
     }
+    
+    // PUT /api/courses/:courseId/reviews/:reviewId
+    // Update review
+    
+    router.put("api", "courses", Course.parameter, "reviews", Int.parameter) { req -> Future<Review> in
+        return try flatMap(to: Review.self, req.parameters.next(Course.self), req.content.decode(Review.self)) { course, updatedReview in
+            
+            let reviewId = try req.parameters.next(Int.self)
+            
+            return try course.reviews.query(on: req).filter(\Review.id == reviewId).first().flatMap(to: Review.self) { filteredReview in
+                
+                guard let filteredReview = filteredReview else {
+                    throw Abort(.notFound, reason: "No review available for specified ID")
+                }
+                
+                filteredReview.rating = updatedReview.rating
+                filteredReview.comment = updatedReview.comment
+                
+                return filteredReview.save(on: req)
+            }
+        }
+    }
+    
+    // DELETE /api/courses/:courseId/reviews/:reviewId
+    // Delete a review
+    
+    router.delete("api", "courses", Course.parameter, "reviews", Int.parameter) { req -> Future<HTTPStatus> in
+        return try req.parameters.next(Course.self).flatMap(to: HTTPStatus.self) { course in
+            let reviewId = try req.parameters.next(Int.self)
+            return try course.reviews.query(on: req).filter(\Review.id == reviewId).first().flatMap(to: HTTPStatus.self) { filteredReview in
+                
+                guard let filteredReview = filteredReview else {
+                    throw Abort(.notFound, reason: "No review available for specified ID")
+                }
+                
+                return filteredReview.delete(on: req).transform(to: HTTPStatus.noContent)
+            }
+        }
+    }
 }
 
 
